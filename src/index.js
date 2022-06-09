a9ad80c216f4ccd0e2adad8f0e0cacefc5b99a40
kleatopuzi <ktopuzii@gmail.com>
2022-06-09 15:37:35 +0200

    remove redux folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 import CartProvider from "./providers/cart/cart.provider";
-import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -13,14 +10,10 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CartProvider>
-    <Provider store={store}>
-      <React.StrictMode>
-        <BrowserRouter>
-          <PersistGate persistor={persistor}>
-            <App />
-          </PersistGate>
-        </BrowserRouter>
-      </React.StrictMode>
-    </Provider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
   </CartProvider>
 );
